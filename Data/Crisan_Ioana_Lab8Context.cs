4700fcec4c1925e4660d08b3a9c418454b9f9b92
Your Name <you@example.com>
2020-11-29 19:03:59 +0200

    Add project files.

---
+++ Data/Crisan_Ioana_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Crisan_Ioana_Lab8.Models;
+
+namespace Crisan_Ioana_Lab8.Data
+{
+    public class Crisan_Ioana_Lab8Context : DbContext
+    {
+        public Crisan_Ioana_Lab8Context (DbContextOptions<Crisan_Ioana_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Crisan_Ioana_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Crisan_Ioana_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Crisan_Ioana_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Crisan_Ioana_Lab8.Data;
+using Crisan_Ioana_Lab8.Models;
+
+namespace Crisan_Ioana_Lab8.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Crisan_Ioana_Lab8.Data.Crisan_Ioana_Lab8Context _context;
+
+        public IndexModel(Crisan_Ioana_Lab8.Data.Crisan_Ioana_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book.Include(b => b.Publisher).ToListAsync();
+            
+        }
+    }
+}
